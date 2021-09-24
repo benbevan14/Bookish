604e9d9c662394e7680a6faaec17c2c59a463916
Benjamin Bevan <bbevan196@gmail.com>
2021-09-24 15:33:29 +0100

    can query db

--- Bookish.ConsoleApp/Program.cs
+++ Bookish.ConsoleApp/Program.cs
@@ -1,12 +1,22 @@
 ﻿using System;
+using System.Linq;
+using System.Data.SqlClient;
+using Dapper;
 
 namespace Bookish.ConsoleApp
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var cs = @"Server=localhost;Database=bookish;Trusted_Connection=True;";
+
+            using var con = new SqlConnection(cs);
+            con.Open();
+
+            var users = con.Query<User>("SELECT * FROM users").ToList();
+
+            users.ForEach(Console.WriteLine);
         }
     }
 }
